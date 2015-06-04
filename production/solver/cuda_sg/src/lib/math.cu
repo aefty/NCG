acbde35f2d5733d77d25ad1deee9b7b8ec365bc3
aefty <aryan.eftekhari@gmail.com>
2015-06-04 14:12:44 +0200

    asdr

--- production/solver/cuda_sg/src/lib/math.cu
+++ production/solver/cuda_sg/src/lib/math.cu
@@ -39,10 +39,13 @@ namespace cuda {
       dim3 GPU_TPB_2D(TPB_OPTIMAL_2D, TPB_OPTIMAL_2D);
       dim3 GPU_BLOCK_2D(N / GPU_TPB_2D.x , (unsigned int)rad / h);
 
+      cout << N / GPU_TPB_2D.x << endl;
+      cout << (unsigned int)rad / h << endl;
+
       double* _x = (double*)cuda::alloc(x);
       double* _p = (double*)cuda::alloc(p);
 
-      discLine_kernel <<<GPU_BLOCK_2D , GPU_TPB_2D>>> (N, _x , _p, h , _space);
+      discLine_kernel <<< GPU_BLOCK_2D , GPU_TPB_2D>>> (N, _x , _p, h , _space);
    };
 
    /*
