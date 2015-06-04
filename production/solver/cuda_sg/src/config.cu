520cc34fa90e223d00abcb6a7ace468f7a8560c0
aefty <aryan.eftekhari@gmail.com>
2015-06-04 22:11:19 +0200

    asdfer

--- production/solver/cuda_sg/src/config.cu
+++ production/solver/cuda_sg/src/config.cu
@@ -18,6 +18,6 @@ __host__ __device__ void FUNCTION(long int N, double* x , double* rtrn ) {
 };
 
 inline void GUESS(long int  N, vector<double>& rtrn) {
-	vector<double>x0 (N, 17);
+	vector<double>x0 (N, 7);
 	rtrn = x0 ;
 };
--- production/solver/cuda_sg/src/ncg.cu
+++ production/solver/cuda_sg/src/ncg.cu
@@ -54,7 +54,7 @@ int main(int argc, char* argv[]) {
 
 
 	int TPB_2D = 16 ;
-	long int range = 16 * 2;
+	long int range = 16 * 2 * 2;
 
 	int block_x = (_GLB_N_ / TPB_2D) < 1 ? 1 : (_GLB_N_ / TPB_2D) ;
 	int block_y = range < 1 ? 1 : range ;
@@ -104,8 +104,8 @@ int main(int argc, char* argv[]) {
 
 			//h  += _GLB_EPS_;
 		redo:
-			gpu::lineDiscretize <<<GPU_BLOCK_2D , GPU_TPB_2D>>>   (_GLB_N_, range, _x0 , _p, h , _space);
-			gpu::lineValue <<< GPU_BLOCK_1D , GPU_TPB_1D>>> (_GLB_N_, range, _space ,  _func_val);
+			gpu::lineDiscretize <<< GPU_BLOCK_2D , GPU_TPB_2D>>>   (_GLB_N_, range, _x0 , _p, h , _space);
+			gpu::lineValue <<<GPU_BLOCK_1D , GPU_TPB_1D>>> (_GLB_N_, range, _space ,  _func_val);
 
 			CUDA_ERR_CHECK(cudaDeviceSynchronize());
 			gpu::unalloc(_func_val, func_val );
