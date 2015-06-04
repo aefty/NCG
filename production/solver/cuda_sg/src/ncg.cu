82b21288cabdf9dce6e8a71e423cbb6581a5438f
aefty <aryan.eftekhari@gmail.com>
2015-06-04 21:15:58 +0200

    asdfer

--- production/solver/cuda_sg/src/ncg.cu
+++ production/solver/cuda_sg/src/ncg.cu
@@ -104,10 +104,10 @@ int main(int argc, char* argv[]) {
 
 			h  = 0.1;
 		redo:
-			gpu::lineDiscretize <<<GPU_BLOCK_2D , GPU_TPB_2D>>>   (_GLB_N_, range, _x0 , _p, h , _space);
-			gpu::lineValue <<< GPU_BLOCK_1D , GPU_TPB_1D>>> (_GLB_N_, range, _space ,  _func_val);
+			gpu::lineDiscretize <<< GPU_BLOCK_2D , GPU_TPB_2D>>>   (_GLB_N_, range, _x0 , _p, h , _space);
+			gpu::lineValue <<<GPU_BLOCK_1D , GPU_TPB_1D>>> (_GLB_N_, range, _space ,  _func_val);
 
-			if (itr > 0) { goto end; }
+
 
 			CUDA_ERR_CHECK(cudaDeviceSynchronize());
 			gpu::unalloc(_func_val, func_val );
@@ -121,6 +121,8 @@ int main(int argc, char* argv[]) {
 			alpha = min_i * h;
 
 			cout << alpha << endl;
+
+			if (itr > 0) { goto end; }
 
 			if (alpha == 0 && h > _GLB_EPS_) {
 				h = h / 2.0;
