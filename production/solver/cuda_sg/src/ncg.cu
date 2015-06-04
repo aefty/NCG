c1b06ccea08c7734ddcaaa963a9fbecd450effcc
aefty <aryan.eftekhari@gmail.com>
2015-06-04 21:58:48 +0200

    asdfer

--- production/solver/cuda_sg/src/ncg.cu
+++ production/solver/cuda_sg/src/ncg.cu
@@ -102,10 +102,10 @@ int main(int argc, char* argv[]) {
 			gpu::alloc(x0, _x0);
 			gpu::alloc(p, _p);
 
-			h  = h * 2.0;
+			h  = h * 1.5;
 		redo:
-			gpu::lineDiscretize <<< GPU_BLOCK_2D , GPU_TPB_2D>>>   (_GLB_N_, range, _x0 , _p, h , _space);
-			gpu::lineValue <<<GPU_BLOCK_1D , GPU_TPB_1D>>> (_GLB_N_, range, _space ,  _func_val);
+			gpu::lineDiscretize <<<GPU_BLOCK_2D , GPU_TPB_2D>>>   (_GLB_N_, range, _x0 , _p, h , _space);
+			gpu::lineValue <<< GPU_BLOCK_1D , GPU_TPB_1D>>> (_GLB_N_, range, _space ,  _func_val);
 
 			CUDA_ERR_CHECK(cudaDeviceSynchronize());
 			gpu::unalloc(_func_val, func_val );
