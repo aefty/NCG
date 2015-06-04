16a133388da011e73f2d6372c7cca548a285005d
aefty <aryan.eftekhari@gmail.com>
2015-06-04 21:25:28 +0200

    asdfer

--- production/solver/cuda_sg/src/ncg.cu
+++ production/solver/cuda_sg/src/ncg.cu
@@ -104,8 +104,8 @@ int main(int argc, char* argv[]) {
 
 			h  = _GLB_EPS_;
 		redo:
-			gpu::lineDiscretize <<< GPU_BLOCK_2D , GPU_TPB_2D>>>   (_GLB_N_, range, _x0 , _p, h , _space);
-			gpu::lineValue <<<GPU_BLOCK_1D , GPU_TPB_1D>>> (_GLB_N_, range, _space ,  _func_val);
+			gpu::lineDiscretize <<<GPU_BLOCK_2D , GPU_TPB_2D>>>   (_GLB_N_, range, _x0 , _p, h , _space);
+			gpu::lineValue <<< GPU_BLOCK_1D , GPU_TPB_1D>>> (_GLB_N_, range, _space ,  _func_val);
 
 
 
@@ -120,7 +120,7 @@ int main(int argc, char* argv[]) {
 
 			alpha = min_i * h;
 
-			//cout << alpha << endl;
+			cout << alpha << endl;
 
 			//if (itr > 0) { goto end; }
 
