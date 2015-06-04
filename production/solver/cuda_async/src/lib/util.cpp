335aa4c449fd0d672a0cb53f91d21b8e942ea1ee
aefty <aryan.eftekhari@gmail.com>
2015-06-04 02:53:26 +0200

    change wout to cout

--- production/solver/cuda_async/src/lib/util.cpp
+++ production/solver/cuda_async/src/lib/util.cpp
@@ -53,28 +53,28 @@ namespace cuda {
 	inline void deviceSpecs() {
 		const int kb = 1024;
 		const int mb = kb * kb;
-		wcout << "NBody.GPU" << endl << "=========" << endl << endl;
+		cout << "NBody.GPU" << endl << "=========" << endl << endl;
 
-		wcout << "CUDA version:   v" << CUDART_VERSION << endl;
+		cout << "CUDA version:   v" << CUDART_VERSION << endl;
 
 		int devCount;
 		cudaGetDeviceCount(&devCount);
-		wcout << "CUDA Devices: " << endl << endl;
+		cout << "CUDA Devices: " << endl << endl;
 
 		for (int i = 0; i < devCount; ++i) {
 			cudaDeviceProp props;
 			cudaGetDeviceProperties(&props, i);
-			wcout << i << ": " << props.name << ": " << props.major << "." << props.minor << endl;
-			wcout << "  Global memory:   " << props.totalGlobalMem / mb << "mb" << endl;
-			wcout << "  Shared memory:   " << props.sharedMemPerBlock / kb << "kb" << endl;
-			wcout << "  Constant memory: " << props.totalConstMem / kb << "kb" << endl;
-			wcout << "  Block registers: " << props.regsPerBlock << endl << endl;
+			cout << i << ": " << props.name << ": " << props.major << "." << props.minor << endl;
+			cout << "  Global memory:   " << props.totalGlobalMem / mb << "mb" << endl;
+			cout << "  Shared memory:   " << props.sharedMemPerBlock / kb << "kb" << endl;
+			cout << "  Constant memory: " << props.totalConstMem / kb << "kb" << endl;
+			cout << "  Block registers: " << props.regsPerBlock << endl << endl;
 
-			wcout << "  Warp size:         " << props.warpSize << endl;
-			wcout << "  Threads per block: " << props.maxThreadsPerBlock << endl;
-			wcout << "  Max block dimensions: [ " << props.maxThreadsDim[0] << ", " << props.maxThreadsDim[1]  << ", " << props.maxThreadsDim[2] << " ]" << endl;
-			wcout << "  Max grid dimensions:  [ " << props.maxGridSize[0] << ", " << props.maxGridSize[1]  << ", " << props.maxGridSize[2] << " ]" << endl;
-			wcout << endl;
+			cout << "  Warp size:         " << props.warpSize << endl;
+			cout << "  Threads per block: " << props.maxThreadsPerBlock << endl;
+			cout << "  Max block dimensions: [ " << props.maxThreadsDim[0] << ", " << props.maxThreadsDim[1]  << ", " << props.maxThreadsDim[2] << " ]" << endl;
+			cout << "  Max grid dimensions:  [ " << props.maxGridSize[0] << ", " << props.maxGridSize[1]  << ", " << props.maxGridSize[2] << " ]" << endl;
+			cout << endl;
 		}
 	}
 };
