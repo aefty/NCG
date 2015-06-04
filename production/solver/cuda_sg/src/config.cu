169d5fb51dcbf2a69e7dfdc47153be9a6a6a78a5
aefty <aryan.eftekhari@gmail.com>
2015-06-04 22:03:46 +0200

    hgjhg

--- production/solver/cuda_sg/src/config.cu
+++ production/solver/cuda_sg/src/config.cu
@@ -6,7 +6,7 @@
 long int _GLB_N_ = 1024 * 1;  // Probelm Size - Note keep at base 2
 long int _GLB_ITR_ = 100;     // Max Solver Iterations
 long int _GLB_ITR_LINE_ = 10; // Max Line search iterationsc
-double _GLB_EPS_ = 1e-5;      // Value of epsilon, Note this is equal to the tolerence (both residual and linesearch)
+double _GLB_EPS_ = 1e-6;      // Value of epsilon, Note this is equal to the tolerence (both residual and linesearch)
 
 
 using namespace std;
@@ -18,6 +18,6 @@ __host__ __device__ void FUNCTION(long int N, double* x , double* rtrn ) {
 };
 
 inline void GUESS(long int  N, vector<double>& rtrn) {
-	vector<double>x0 (N, 7);
+	vector<double>x0 (N, 17);
 	rtrn = x0 ;
 };
