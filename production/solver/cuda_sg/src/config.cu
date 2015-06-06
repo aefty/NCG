4f461954ec82274f08727e5f2b9bf717d7157ba9
aefty <aryan.eftekhari@gmail.com>
2015-06-06 12:38:52 +0200

    fer

--- production/solver/cuda_sg/src/config.cu
+++ production/solver/cuda_sg/src/config.cu
@@ -4,13 +4,13 @@
  */
 
 long int _GLB_N_ = 5 * 1;  // Probelm Size - Note keep at base 2
-long int _GLB_ITR_ = 10;     // Max Solver Iterations
+long int _GLB_ITR_ = 1;     // Max Solver Iterations
 long int _GLB_ITR_LINE_ = 0;  // Not relevant here
 double _GLB_EPS_ = 1e-6;      // Value of epsilon, Note this is equal to the tolerence (both residual and linesearch)
 
 using namespace std;
 
-__host__ __device__ void FUNCTION(long int N, double* x , double* rtrn ) {
+__host__ __device__ void __FUNCTION(long int N, double* x , double* rtrn ) {
 	for (int i = 0; i <  N - 1; ++i) {
 		rtrn[0] += 100 * (x[i + 1] - x[i] * x[i]) * (x[i + 1] - x[i] * x[i]) + (1 - x[i]) * (1 - x[i]);
 	};
@@ -20,6 +20,12 @@ __host__ __device__ void FUNCTION(long int N, double* x , double* rtrn ) {
 __host__ __device__ void _FUNCTION(long int N, double* x , double* rtrn ) {
 	for (int i = 0; i <  N; ++i) {
 		rtrn[0] += x[i]*x[i];
+	};
+};
+
+__host__ __device__ void FUNCTION(long int N, double* x , double* rtrn ) {
+	for (int i = 0; i <  N; ++i) {
+		rtrn[0] += x[i];
 	};
 };
 
