94d4397ec37e9ca724111a8721012d9068455f9b
aefty <aryan.eftekhari@gmail.com>
2015-06-06 14:19:07 +0200

    asdf

--- production/solver/cuda_sg/src/lib/lineSearch.cu
+++ production/solver/cuda_sg/src/lib/lineSearch.cu
@@ -24,7 +24,7 @@ namespace gpu {
 
          //     printf("i : %d, row :%d , col %d \n", i, row, col );
          //  space[row * N + col] = x[col] + p[col] * h * row;
-         space[row * N + col] = x[col] + p[col] * h * (pow(1.5, row) - 1.0);
+         space[row * N + col] = x[col] + p[col] * h * (__device__ pow(1.5, row) - 1.0);
 
       }
    };
