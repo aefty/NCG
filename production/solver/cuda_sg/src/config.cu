21df1bac4d97be2011f8690f39f69920616d542d
aefty <aryan.eftekhari@gmail.com>
2015-06-06 13:49:43 +0200

    asdfe

--- production/solver/cuda_sg/src/config.cu
+++ production/solver/cuda_sg/src/config.cu
@@ -3,8 +3,8 @@
  * Global configuration of the solver including the function and intial value are specfied.
  */
 
-long int _GLB_N_ = 1 * 1;  // Probelm Size - Note keep at base 2
-long int _GLB_ITR_ = 10;     // Max Solver Iterations
+long int _GLB_N_ = 4 * 1;  // Probelm Size - Note keep at base 2
+long int _GLB_ITR_ = 1;     // Max Solver Iterations
 long int _GLB_ITR_LINE_ = 0;  // Not relevant here
 double _GLB_EPS_ = 1e-6;      // Value of epsilon, Note this is equal to the tolerence (both residual and linesearch)
 
