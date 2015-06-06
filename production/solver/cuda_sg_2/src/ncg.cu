cae2801c1d856396d40870b6705634e3fe007df7
aefty <aryan.eftekhari@gmail.com>
2015-06-06 02:18:26 +0200

    asdfer

--- production/solver/cuda_sg_2/src/ncg.cu
+++ production/solver/cuda_sg_2/src/ncg.cu
@@ -93,14 +93,14 @@ int main(int argc, char* argv[]) {
 	// BEGIN NCG
 	{
 		//cpu::linalg_grad(_GLB_N_, _GLB_EPS_, x0, p);
-		gpu::spcc <<< nn_blocks , nn_tpb>>>   (_GLB_N_, _x0, _gr_space );
-		gpu::grad <<< nn_blocks , nn_tpb>>>   (_GLB_N_, _GLB_EPS_, _gr_space , _p);
+		gpu::spcc <<<nn_blocks , nn_tpb>>>   (_GLB_N_, _x0, _gr_space );
+		gpu::grad <<<nn_blocks , nn_tpb>>>   (_GLB_N_, _GLB_EPS_, _gr_space , _p);
 
 		//cpu::linalg_sdot( -1.0, p, p);
-		gpu::axpby <<< ln_tpb , ln_blocks>>>  (_GLB_N_, -1.0, _p , 0.0, _p , _p);
+		gpu::axpby <<<ln_tpb , ln_blocks>>>  (_GLB_N_, -1.0, _p , 0.0, _p , _p);
 
 		//cpu::linalg_dot(p, p, _vtemp);
-		gpu::dot <<< ln_tpb , ln_blocks>>>    (_GLB_N_, _p, _p , _vtemp);
+		gpu::dot <<<ln_tpb , ln_blocks>>>    (_GLB_N_, _p, _p , _vtemp);
 		CUDA_ERR_CHECK(cudaDeviceSynchronize());
 		gpu::unalloc(_vtemp, vtemp);
 		gg0 = 0;
@@ -108,7 +108,6 @@ int main(int argc, char* argv[]) {
 		for (int i = 0; i < _GLB_N_; ++i) {
 			gg0 += vtemp[i];
 		};
-
 
 		// Pointer Swap
 		_x1 = _x0;
@@ -130,16 +129,9 @@ int main(int argc, char* argv[]) {
 			*/
 			{
 
-
-				gpu::spcl <<< ln_blocks , ln_tpb>>> (_GLB_N_, range, _x0 , _p, h , _ld_space);
-
-				gpu::fv <<< nm_blocks , nm_tpb>>> (_GLB_N_, range, _ld_space ,  _vtempl);
-
-				CUDA_ERR_CHECK(cudaDeviceSynchronize());
-
+				gpu::spcl <<<ln_blocks , ln_tpb>>> (_GLB_N_, range, _x0 , _p, h , _ld_space);
+				gpu::fv <<<nm_blocks , nm_tpb>>> (_GLB_N_, range, _ld_space ,  _vtempl);
 				gpu::unalloc(_vtempl, vtempl );
-				CUDA_ERR_CHECK(cudaDeviceSynchronize());
-
 
 				for (int i = 1; i < vtempl.size(); i++) {
 					if (vtempl[i] < vtempl[min_i]) {
@@ -150,8 +142,6 @@ int main(int argc, char* argv[]) {
 				alpha = min_i * h;
 			}
 			// END LINE SEARCH
-			cout << alpha << endl;
-
 
 			/**
 			* CODE BLOCK 3
@@ -159,18 +149,16 @@ int main(int argc, char* argv[]) {
 			*/
 
 			//cpu::linalg_add (1.0, x0, alpha, p, x1);
-			gpu::axpby <<< ln_blocks , ln_tpb>>> (_GLB_N_, 1.0, _x0 , alpha, _p , _x1);
+			gpu::axpby <<<ln_blocks , ln_tpb>>> (_GLB_N_, 1.0, _x0 , alpha, _p , _x1);
 
 			t_lineSearch += (clock() - t_lineSearch_start) / (double) CLOCKS_PER_SEC;
 
 			//cpu::linalg_grad(_GLB_N_, _GLB_EPS_, x1, g1);
-			gpu::spcc <<< nn_blocks , nn_tpb>>> (_GLB_N_, _x1, _gr_space );
-			gpu::grad <<< ln_blocks , ln_tpb>>> (_GLB_N_, _GLB_EPS_, _gr_space , _g1);
-
-
+			gpu::spcc <<<nn_blocks , nn_tpb>>> (_GLB_N_, _x1, _gr_space );
+			gpu::grad <<<ln_blocks , ln_tpb>>> (_GLB_N_, _GLB_EPS_, _gr_space , _g1);
 
 			//cpu::linalg_dot(g1, g1, gg1);
-			gpu::dot <<< ln_blocks , ln_tpb>>>  (_GLB_N_, _g1, _g1 , _vtemp);
+			gpu::dot <<<ln_blocks , ln_tpb>>>  (_GLB_N_, _g1, _g1 , _vtemp);
 			CUDA_ERR_CHECK(cudaDeviceSynchronize());
 			gpu::unalloc(_vtemp, vtemp);
 			gg1 = 0;
@@ -179,22 +167,18 @@ int main(int argc, char* argv[]) {
 				gg1 += vtemp[i];
 			};
 
-
 			B = gg1 / gg0;
 
 			//% p = -g1 + B * p;
 			//cpu::linalg_add(-1.0, g1, B, p, p);
-			gpu::axpby <<< ln_blocks , ln_tpb>>> (_GLB_N_, -1.0, _g1 , B, _p , _p);
+			gpu::axpby <<<ln_blocks , ln_tpb>>> (_GLB_N_, -1.0, _g1 , B, _p , _p);
 
 			//% tol = norm(x1 - x0)
 			//cpu::linalg_add(1.0, x1, -1.0, x0, vtemp);
-			gpu::axpby <<< ln_blocks , ln_tpb>>> (_GLB_N_, 1.0, _x1 , -1.0, _x0 , _vtemp);
+			gpu::axpby <<<ln_blocks , ln_tpb>>> (_GLB_N_, 1.0, _x1 , -1.0, _x0 , _vtemp);
 
 			//cpu::linalg_dot(vtemp, vtemp, tol);
-			gpu::dot <<< ln_blocks , ln_tpb>>> (_GLB_N_, _vtemp, _vtemp , _vtemp);
-
-
-			CUDA_ERR_CHECK(cudaDeviceSynchronize());
+			gpu::dot <<<ln_blocks , ln_tpb>>> (_GLB_N_, _vtemp, _vtemp , _vtemp);
 
 			gpu::unalloc(_vtemp, vtemp);
 
@@ -211,13 +195,9 @@ int main(int argc, char* argv[]) {
 
 			gg0 = gg1;
 
-			_x0 = _x1;
+			//_x0 = _x1;
 
-			//temp = *_x0;
-
-			//*_x0 = *_x1;
-
-			//*_x1 = temp;
+			gpu::axpby <<<ln_blocks , ln_tpb>>> (_GLB_N_, 1.0, _x1 , 0.0, _x1 , _x0);
 
 			itr ++;
 		}
