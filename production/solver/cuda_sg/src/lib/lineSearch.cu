1bd660b2754c4a612ed92737b2cc21dbc88d1e32
aefty <aryan.eftekhari@gmail.com>
2015-06-06 14:33:45 +0200

    fer

--- production/solver/cuda_sg/src/lib/lineSearch.cu
+++ production/solver/cuda_sg/src/lib/lineSearch.cu
@@ -25,7 +25,7 @@ namespace gpu {
          //     printf("i : %d, row :%d , col %d \n", i, row, col );
          //  space[row * N + col] = x[col] + p[col] * h * row;
 
-         double base = 1.5;
+         double base = 1.1;
          double power = 1;
 
          for (int j = 0; j < row; ++j) {
