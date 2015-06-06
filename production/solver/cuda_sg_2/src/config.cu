675b2b6e76c10e7b98c9c13c3d2f9f27e4d8a1e1
aefty <aryan.eftekhari@gmail.com>
2015-06-06 10:34:07 +0200

    asdfer

--- production/solver/cuda_sg_2/src/config.cu
+++ production/solver/cuda_sg_2/src/config.cu
@@ -10,9 +10,17 @@ double _GLB_EPS_ = 1e-6;      // Value of epsilon, Note this is equal to the tol
 
 using namespace std;
 
-__host__ __device__ void FUNCTION(long int N, double* x , double* rtrn ) {
+__host__ __device__ void _FUNCTION(long int N, double* x , double* rtrn ) {
 	for (int i = 0; i <  N - 1; ++i) {
 		rtrn[0] += 100 * (x[i + 1] - x[i] * x[i]) * (x[i + 1] - x[i] * x[i]) + (1 - x[i]) * (1 - x[i]);
+	};
+};
+
+
+
+__host__ __device__ void FUNCTION(long int N, double* x , double* rtrn ) {
+	for (int i = 0; i <  N ; ++i) {
+		rtrn[0] += x[i]x[i];
 	};
 };
 
