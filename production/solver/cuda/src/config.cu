915cf848bcb4f170baec11bca2277ba2028ccf14
aefty <aryan.eftekhari@gmail.com>
2015-06-07 23:49:58 +0200

    Final

--- production/solver/cuda/src/config.cu
+++ production/solver/cuda/src/config.cu
@@ -31,7 +31,7 @@ __device__ void FUNCTION(long int N, double* x , double* rtrn ) {
  * @param x    [description]
  * @param rtrn [description]
  */
-__device__ void FUNCTION(long int N, double* x , double* rtrn ) {
+__device__ void _FUNCTION(long int N, double* x , double* rtrn ) {
 	for (int i = 0; i <  N ; ++i) {
 		rtrn[0] += (x[i]-1.0) / (exp ((x[i]-1.0)/.35)-1.0);
 	};
