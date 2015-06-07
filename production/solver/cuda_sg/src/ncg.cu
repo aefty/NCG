b6f394f717058080832c108ce10adcbc4a5ba435
aefty <aryan.eftekhari@gmail.com>
2015-06-07 14:18:22 +0200

    asder

--- production/solver/cuda_sg/src/ncg.cu
+++ production/solver/cuda_sg/src/ncg.cu
@@ -103,8 +103,8 @@ int main(int argc, char* argv[]) {
 				gpu::alloc(x0, _x0);
 				gpu::alloc(p, _p);
 
-				gpu::spcl <<<threadsPerBlock_spcl , numBlocks_spcl>>>   (_GLB_N_, range, _x0 , _p, h , _space);
-				gpu::lineSearch   <<<threadsPerBlock_fval  , numBlocks_fval>>> (_GLB_N_, range, _space ,  _func_val);
+				gpu::spcl <<< threadsPerBlock_spcl , numBlocks_spcl>>>   (_GLB_N_, range, _x0 , _p, h , _space);
+				gpu::lineSearch   <<< threadsPerBlock_fval  , numBlocks_fval>>> (_GLB_N_, range, _space ,  _func_val);
 
 				CUDA_ERR_CHECK(cudaDeviceSynchronize());
 				gpu::unalloc(_func_val, func_val );
@@ -168,6 +168,7 @@ int main(int argc, char* argv[]) {
 	//json.append("alpha", alhpa_history);
 	json.append("m_history", m_history);
 	json.append("func_val", func_val);
+	json.append("space", space);
 
 	if (showX) {
 		json.append("x", x1);
