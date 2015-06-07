3a3972c91462fcd328dc9333410c40ff0a128b44
aefty <aryan.eftekhari@gmail.com>
2015-06-07 14:22:54 +0200

    asder

--- production/solver/cuda_sg/src/ncg.cu
+++ production/solver/cuda_sg/src/ncg.cu
@@ -67,7 +67,7 @@ int main(int argc, char* argv[]) {
 
 	vector<double> space(range * _GLB_N_, 0.0); double* _space = (double*) gpu::alloc(space);
 	dim3 threadsPerBlock_spcl(128);
-	dim3 numBlocks_spcl(_GLB_N_ / 128);
+	dim3 numBlocks_spcl(_GLB_N_ / 128 + 1);
 
 	cout << _GLB_N_ * range / threadsPerBlock_spcl.x + 1;
 
